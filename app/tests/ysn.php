91b37e994b4c78830e3793a0852bf3c333bd295b
cclites <cclites@sweeps-soft.com>
2014-04-06 08:56:14 -0500

    Initial commit

---
+++ app/tests/ysn.php
@@ -0,0 +1,14 @@
+<?php
+
+    class YSN_initTests extends TestCase{
+    	
+		//expects a return value of zero from the init function.
+		// Indicates that Init::init completed
+		
+	    public function testInitReturn(){
+	    	
+			$this->assertTrue(true);
+	    }
+		
+    }
+?>

d83b9fec3a03fae192465c5769a4eeed3077dfc7
cclites <cclites@sweeps-soft.com>
2014-05-14 09:54:53 -0500

    Admin page
    -Completed form, including message length count.

--- public/js/admin_controller.js
+++ public/js/admin_controller.js
@@ -19,25 +19,37 @@ var ysk_admin = {
 	  
 	    //This does not remove the spaces, so use a loop to set 
 	    //a count on the fly.
-	    //status = status.replace(' ', '');
+	    //status = status.replace(' ', '');  // Doesn't work. Why?
 	    
 	    var count = 0;
-	    
 	    for(var i = 0; i < status.length; i += 1){
 	    	if( status[i] !== " "  && status[i] !== '\n'){
 	    		count += 1;
 	    	}
 	    }
-	  
-	    //alert("->" + status.length + "<-");
-	    //alert(count);
 	
 	    //Theoretically should not be able to go over 120 chars
 	    if(count < 121){
 	  	    Model.chars(120 - count);
 	    }
 	    else{
-	  	    Model.status( status.substring(0, 121));
+	    	
+	    	var strLen = 0;
+	    	var charCnt = 0;
+	    	
+            for(var i = 0; charCnt < 121; i += 1){
+            	
+    	        if( status[i] !== " "  && status[i] !== '\n' ){
+    		        charCnt += 1;
+    	        }
+    	        
+    	        strLen += 1;
+    	    }
+
+	  	    Model.status( status.substr(0, strLen));
 	    }
 	}
+	
+	
+	
 };

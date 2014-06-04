5cb7b96d2de77e94d9c77e94ca508e7b76f50282
cclites <cclites@sweeps-soft.com>
2014-06-03 19:46:12 -0500

    ui tweaks
    Minor un-notable tweaks

--- app/views/default/default.blade.php
+++ app/views/default/default.blade.php
@@ -1,17 +1,15 @@
 <div class="default container">
     <article >
-	    Hello! I am a web developer, and this is a project I started last year in order to
-	    evaluate a Javascript framework. I recently started it again from scratch, this time
-	    to evaluate a PHP framework.
+	    Hello! Welcome! This is a little project that I have been working on that caters to a weird desire to know how those in Congress are representing my interests.
 	    <br><br>
-	    The app pulls Congressional voting data from GovTrack.us, and pushes it out via Twitter
-	    accounts, one for each state. For example: 
+	    It works like this: The app pulls Congressional voting data from GovTrack.us, and pushes it out via Twitter accounts, one for each state. For example: 
 	    <br>  
 	    <a href="https://twitter.com/sdshouldknow" target="_blank">sdshouldknow - South Dakota</a>
 	    <br>
 	    <a href="https://twitter.com/mnshouldknow" target="_blank">msshouldknow - Minnesota</a>
+	    <br>
+	    The status updates at Twitter include a link back to ushouldknow.us with some information about the vote, the result, and any other relevant bill info. As it progresses, I am hoping to include information from more resources. Data is displayed more or less as it is retrieved, and is emperical in nature. The same data is available from GovTrack.us.
 	    <br><br>
-	    Honestly, I have no idea yet what I am going to do with this. Everything here flows from Twitter,
-	    and data is displayed just as it is retrieved.
+	    I am open to new ideas. There is a contact link on the bottom right of the page. Use it if you feel inclined.
     </article>
 </div>
